74163487b27a790cbd0f81b46dea909d96d2f78a
Paul Hohenberger <49005843+phohenberger@users.noreply.github.com>
2023-09-22 12:17:30 +0200

    Update visualizer_zndraw.py

--- src/python/espressomd/visualizer_zndraw.py
+++ src/python/espressomd/visualizer_zndraw.py
@@ -6,7 +6,7 @@ of Espresso. The speed is still extremely slow for large quantities of
 particles.
 
 -- Plans to be implemented
-- automatic detection of position/size
+
 - atomic bonds
 - obstacles
 - speed improvement
@@ -21,7 +21,7 @@ where it previously crashed is still open.
 (Especially chrome)
 
 ----------------
--- How to use --                                                                                                                                  -Before the simulation loop, insert the setup line:                                                                                              vis = visualizer_zndraw.setup_vis()
+-- How to use --                                                                                                                                                                                                                    vis = visualizer_zndraw.setup_vis()
 
 -First you need to install zndraw on your machine. To do this run the following
 -command in your console
@@ -40,7 +40,8 @@ vis = visual.setup()
 -If you have a state you want to visualize use                                                                                                    
 
 visual.update_single(vis, system, size)  
-                                                                                                                                                                                                     
+
+- "system" is your espressomd.system object                                                                                                                                                                                                    
 -The size parameter is optional, if left empty all particles appear in the same                                              
 -color an size. If you want to customize the size for each particle, input an                                                
 -numpy array where you assign each particle an integer size. The length of the size-array                                    
@@ -62,8 +63,8 @@ def setup():
     -------
     visZn : ZnDraw()
         returns Visualizer object ZnDraw()
-
     """
+  
     visZn = ZnDraw()
     visZn.socket.sleep(2)
     return visZn
@@ -84,7 +85,6 @@ def espresso_to_atoms(pos, size=None):
     -------
     atoms_obj : ase.atoms 
         Data is saved as an ase.atoms object
-
     """
     
     if size == None:
@@ -109,11 +109,12 @@ def update_single(visualizer, system, size=None):
         size of particles. The default is None. If none then all particles
         have the same size
     """
+  
     pos = system.part.all().pos
     vis_atoms = espresso_to_atoms(pos, size)
     visualizer.append(vis_atoms)
     
-#def vis_update_all_after(visualizer, pos, size=None):
+
     
 
 
